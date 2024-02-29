ee15ecf68a07882548aaf2c09226abff7eb524ba
TadhgRooney <124795825+TadhgRooney@users.noreply.github.com>
2024-02-29 16:29:56 +0000

    Main menu
    Main menu added along with a switch statement  to access other options.

--- showRoom.c
+++ showRoom.c
@@ -2,7 +2,7 @@
 
 
 #include <stdio.h>// Standard input output library
-#include <stdio.h> 
+#include <stdlib.h> 
 
 struct car {
 
@@ -30,28 +30,124 @@ char review[20];
 };
 
 //function prototypes
-void addCar();
-void sellCar();
-void reserved();
-void viewCars();
-void viewOne();
-void reviews();
-void exit();
+void menu();
+
+void addCar(); //1
+void sellCar(); //2
+void reserved(); //3
+void viewCars(); //4 
+void viewOne(); //5 
+void reviews(); //6
+//void exit(); //7
+
+
 
 
 int main(){
 
-    int option;
+    menu();
 
-    printf("Welcome to the Showroom");
+    //Notes for Git hub
+    //I made a switch statment 
+    //Something wrong with exit();
+
+    
+
+        
+    
+
+
+}
+
+
+void menu(){
+
+    int option;
+    
+     printf("Welcome to the Showroom");
 
     printf("\n\tPlease pick an option");
 
     printf("\n\t 1.Add a car\n\t 2.Sell a car\n\t 3.Reserve/unreserve a car\n\t 4.View cars\n\t 5.View a specific car\n\t 6.Reviews\n\t 7.Exit");
-    scanf("%d", option);
+    scanf("%d", &option);
 
-    //if statments for each option or switch statment
+     switch(option){
+        case 1:
+        addCar();
+        break;
+
+        case 2:
+        sellCar();
+        break;
+
+        case 3:
+        reserved();
+        break;
+
+        case 4:
+        viewCars();
+        break;
+
+        case 5:
+        viewOne();
+        break;
+
+        case 6:
+        reviews();
+        break;
+
+      // case 7:
+     //  exit();
+       // break;
+
+        default:
+
+        printf("Error, pick an option\n");
+
+        menu();
+
+        
+    }
 
 
 
 }
+
+void addCar(){
+    printf("add");
+}
+
+void sellCar(){
+    printf("sell");
+}
+
+
+void reserved(){
+    printf("reserved");
+}
+
+void viewCars(){
+    printf("view cars");
+}
+
+void viewOne(){
+    printf("view one");
+}
+
+void reviews(){
+    printf("reviews");
+}
+
+//void exit(){
+  // printf("Hello");
+
+  // exit(0);
+//}
+
+
+
+
+
+
+
+
